c19053e6b12f0b16f9d36341fdf7eabdcf1bb463
lepatou76 <patrice.ambert@wanadoo.fr>
2023-04-25 09:55:34 +0200

    premier commit du projet test. Affichage message console "Hello"

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Patrice
+ *
+ */
+module test {
+}
---
+++ src/test/ClassTest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class ClassTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
